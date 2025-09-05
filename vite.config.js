a81fa2c59863dd8e9969df9f43294cd3ff1a66b7
Carlos Fernando Riaño Bedoya <carlosestecnologia@gmail.com>
2025-09-05 11:45:31 +0000

    404 corregido

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Mindado/',   // 👈 muy importante (nombre exacto de tu repo)
 })
